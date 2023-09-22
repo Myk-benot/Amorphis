c4a0f7ea23653ea57b216933cdf0f2a69e552fd1
Myk Benot <sirbenot76@gmail.com>
2023-09-22 13:17:35 +0200

    album/page setup

--- src/components/Albums.jsx
+++ src/components/Albums.jsx
@@ -3,10 +3,287 @@ import React from "react";
 export default function Albums() {
   return (
     <>
-      <div class="bg-gradient-to-r from-sky-800 to-sky-950 h-screen text-yellow-50 font-mono">
-        <img></img>
-        <div>
-          <h1>Albums</h1>
+      <div class="text-yellow-50 font-mono bg-bg-amorphis h-full  bg-gradient-to-r from-sky-800 to-sky-950 mx-auto">
+        <h1 class="text-center py-16 text-2xl">Albums</h1>
+        <div class="grid grid-col-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-fit mx-auto py-8">
+          <div class="border p-2 rounded-lg">
+            <p>
+              <a href=""></a>
+              The Karelian Isthmus
+              <br />
+              Released: 1 November 1992
+              <br />
+              Label: Relapse
+              <br />
+              Formats: CD, CS, LP, DL
+              <br />— — — — — — — — —
+            </p>
+          </div>
+
+          <div class="border p-2 rounded-lg">
+            <p>
+              <a href=""></a>
+              Tales from the Thousand Lakes
+              <br />
+              Released: 12 July 1994
+              <br />
+              Label: Relapse Records
+              <br />
+              Formats: CD, CS, LP, DL
+              <br />
+              — — — — — — — — —<br />
+              Worldwide: 250,000[10]
+            </p>
+          </div>
+
+          <div class="border p-2 rounded-lg">
+            <p>
+              <a href=""></a>
+              Elegy
+              <br />
+              Released: 14 May 1996
+              <br />
+              Label: Relapse
+              <br />
+              Formats: CD, CS, LP, DL
+              <br />
+              8 67 78 — — — — — —<br />
+              Worldwide: 130,000[11]
+              <br />
+              US: 15,100[12]
+            </p>
+          </div>
+
+          <div class="border p-2 rounded-lg">
+            <p>
+              <a href=""></a>
+              Tuonela
+              <br />
+              Released: 29 March 1999
+              <br />
+              Label: Relapse
+              <br />
+              Formats: CD, CS, LP, DL
+              <br />
+              8 46 — — — — — — —<br />
+              US: 12,765[12]
+            </p>
+          </div>
+
+          <div class="border p-2 rounded-lg">
+            <p>
+              <a href=""></a>
+              Am Universum
+              <br />
+              Released: 3 April 2001
+              <br />
+              Label: Relapse
+              <br />
+              Formats: CD, CS, LP, DL
+              <br />
+              4 75 — — — — — — —<br />
+              US: 7,757[12]
+            </p>
+          </div>
+
+          <div class="border p-2 rounded-lg">
+            <p>
+              <a href=""></a>
+              Far from the Sun
+              <br />
+              Released: 26 May 2003
+              <br />
+              Label: Virgin, EMI
+              <br />
+              Formats: CD, CS, LP, DL
+              <br />
+              7 — — — — — — — —<br />
+              FIN: 15,000[13]
+              <br />
+              FIN: Gold[13]
+            </p>
+          </div>
+
+          <div class="border p-2 rounded-lg">
+            <p>
+              <a href=""></a>
+              Eclipse
+              <br />
+              Released: 15 February 2006
+              <br />
+              Label: Nuclear Blast
+              <br />
+              Formats: CD, LP, DL
+              <br />
+              1 56 160 69 — 29 — — —<br />
+              FIN: 16,247[14]
+              <br />
+              US: 700[15]
+              <br />
+              FIN: Gold[14]
+            </p>
+          </div>
+
+          <div class="border p-2 rounded-lg">
+            <p>
+              <a href=""></a>
+              Silent Waters
+              <br />
+              Released: 29 August 2007
+              <br />
+              Label: Nuclear Blast
+              <br />
+              Formats: CD, LP, DL
+              <br />
+              3 44 74 49 52 — — — —<br />
+              FIN: 19,998[14]
+              <br />
+              FIN: Gold[14]
+            </p>
+          </div>
+
+          <div class="border p-2 rounded-lg">
+            <p>
+              <a href=""></a>
+              Skyforger
+              <br />
+              Released: 27 May 2009
+              <br />
+              Label: Nuclear Blast
+              <br />
+              Formats: CD, LP, DL
+              <br />
+              1 55 65 74 48 — — — 33
+              <br />
+              FIN: 18,620[14]
+              <br />
+              US: 1,300[16]
+              <br />
+              FIN: Gold[14]
+            </p>
+          </div>
+
+          <div class="border p-2 rounded-lg">
+            <p>
+              <a href=""></a>
+              The Beginning of Times
+              <br />
+              Released: 25 May 2011
+              <br />
+              Label: Nuclear Blast
+              <br />
+              Formats: CD, LP, DL
+              <br />
+              1 16 37 41 32 — — — 25
+              <br />
+              US: 1,800[17][18]
+            </p>
+          </div>
+
+          <div class="border p-2 rounded-lg">
+            <p>
+              <a href=""></a>
+              Circle
+              <br />
+              Released: 19 April 2013
+              <br />
+              Label: Nuclear Blast
+              <br />
+              Formats: CD, CD+DVD, LP, DL
+              <br />
+              1 13 39 24 57 13 33 141 11
+              <br />
+              FIN: 10,000[19]
+              <br />
+              US: 2,000[20][21][22]
+              <br />
+              FIN: Gold[19]
+            </p>
+          </div>
+
+          <div class="border p-2 rounded-lg">
+            <p>
+              <a href=""></a>
+              Under the Red Cloud
+              <br />
+              Released: 4 September 2015
+              <br />
+              Label: Nuclear Blast
+              <br />
+              Formats: CD, CS, LP, DL
+              <br />2 10 38 20 11 25 — 85 5
+            </p>
+          </div>
+
+          <div class="border p-2 rounded-lg">
+            <p>
+              <a href=""></a>
+              Queen of Time
+              <br />
+              Released: 18 May 2018
+              <br />
+              Label: Nuclear Blast
+              <br />
+              Formats: CD, LP, DL
+              <br />
+              1 4 45 11 3 16 45 84 3<br />
+              FIN: 10,000[23]
+              <br />
+              FIN: Gold[23]
+            </p>
+          </div>
+
+          <div class="border p-2 rounded-lg">
+            <p>
+              <a href=""></a>
+              Halo
+              <br />
+              Released: 11 February 2022
+              <br />
+              Label: Atomic Fire
+              <br />
+              Formats: CD, LP, DL
+            </p>
+          </div>
+
+          <div class="border p-2 rounded-lg">
+            <p>
+              <a href=""></a>
+              Privilege of Evil
+              <br />
+              Released: 5 December 1993
+              <br />
+              Label: Relapse
+              <br />
+              Formats: CD, CS, LP
+            </p>
+          </div>
+
+          <div class="border p-2 rounded-lg">
+            <p>
+              <a href=""></a>
+              Black Winter Day
+              <br />
+              Released: 31 January 1995
+              <br />
+              Label: Relapse
+              <br />
+              Formats: CD, LP, DL
+            </p>
+          </div>
+
+          <div class="border p-2 rounded-lg">
+            <p>
+              <a href=""></a>
+              My Kantele
+              <br />
+              Released: 27 May 1997
+              <br />
+              Label: Relapse
+              <br />
+              Formats: CD, CS, DL
+            </p>
+          </div>
         </div>
       </div>
     </>
