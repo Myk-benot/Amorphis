7a6b7717eb4f08556d2569a84be1dd4b181be02b
Myk Benot <sirbenot76@gmail.com>
2023-09-27 10:09:21 +0200

    token

--- src/components/Spotify.jsx
+++ src/components/Spotify.jsx
@@ -55,7 +55,7 @@ export default function Spotify() {
       try {
         const { data } = await axios.get("https://api.spotify.com/v1/search", {
           headers: {
-            Authorization: `Bearer ${token}`,
+            Authorization: `Bearer ${accessToken}`,
           },
           params: {
             q: "Amorphis",
@@ -75,7 +75,7 @@ export default function Spotify() {
       }
     };
 
-    if (token) {
+    if (accessToken) {
       searchAmorphisSongs();
     }
   }, [token]);
